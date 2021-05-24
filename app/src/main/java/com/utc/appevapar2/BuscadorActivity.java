b9fac04e5c7f7f2479d6878130b59d946b5a59f7
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2021-05-24 01:20:15 -0500

    SharePreferences AVaca

--- app/src/main/java/com/utc/appevapar2/BuscadorActivity.java
+++ app/src/main/java/com/utc/appevapar2/BuscadorActivity.java
@@ -2,11 +2,14 @@ package com.utc.appevapar2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BuscadorActivity extends AppCompatActivity {
@@ -14,60 +17,72 @@ public class BuscadorActivity extends AppCompatActivity {
     EditText txtBarraBuscador; //Definicion de variable para capturar lo que el usuario va a busccar
     WebView wvResultadosBuscador; //Definicion de variable para mostrar el lugar solicitado por el usuario
 
+
+    // Prepara un string para recibir los datos del preference
+
+    String ciudad;
+
     //Proceso 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscador);
+        // Recuperamos datos del share preferences
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        // Asignar a la variable el valor del preference
+        ciudad = preferences.getString("ciudad", "");
+
         //Mapeo de elementos
         txtBarraBuscador = (EditText) findViewById(R.id.txtBarraBuscador); //Mapear el elemento txtBarraBuscador
         wvResultadosBuscador = (WebView) findViewById(R.id.wvResultadosBuscador); //Mapear el elemento WebView
+
 
     }
     //Proceso 2
 
     public void buscarSitio(View vista) {
-        String lugarIngresado = "https://www.google.com.ec/maps/search/"+ this.txtBarraBuscador.getText().toString(); //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
+        String lugarIngresado = "https://www.google.com.ec/maps/search/" + this.txtBarraBuscador.getText().toString() + "+" + this.ciudad; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
         wvResultadosBuscador.setWebViewClient(new WebViewClient()); //Creando un navegador nuevo cada ves que el usuario ingrese un sitio a buscar
         wvResultadosBuscador.getSettings().setJavaScriptEnabled(true); //Habilitando JavaScript dentro de las paginas que se esten buscando
         wvResultadosBuscador.getSettings().setDomStorageEnabled(true); //Habilitando todos los elementos del Doom del sitio solicitado
         wvResultadosBuscador.loadUrl(lugarIngresado); //Cargando en el navegador el sitio ingresado por el usuario
-        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....",Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
+        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....", Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
     }
 
     public void buscarSugerencia1(View vista) {
-        String lugarIngresado = "https://www.google.com.ec/maps/search/cafeterias"; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
+        String lugarIngresado = "https://www.google.com.ec/maps/search/cafeterias+" + this.ciudad; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
         wvResultadosBuscador.setWebViewClient(new WebViewClient()); //Creando un navegador nuevo cada ves que el usuario ingrese un sitio a buscar
         wvResultadosBuscador.getSettings().setJavaScriptEnabled(true); //Habilitando JavaScript dentro de las paginas que se esten buscando
         wvResultadosBuscador.getSettings().setDomStorageEnabled(true); //Habilitando todos los elementos del Doom del sitio solicitado
         wvResultadosBuscador.loadUrl(lugarIngresado); //Cargando en el navegador el sitio ingresado por el usuario
-        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....",Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
+        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....", Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
     }
 
     public void buscarSugerencia2(View vista) {
-        String lugarIngresado = "https://www.google.com.ec/maps/search/bares"; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
+        String lugarIngresado = "https://www.google.com.ec/maps/search/bares+" + this.ciudad; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
         wvResultadosBuscador.setWebViewClient(new WebViewClient()); //Creando un navegador nuevo cada ves que el usuario ingrese un sitio a buscar
         wvResultadosBuscador.getSettings().setJavaScriptEnabled(true); //Habilitando JavaScript dentro de las paginas que se esten buscando
         wvResultadosBuscador.getSettings().setDomStorageEnabled(true); //Habilitando todos los elementos del Doom del sitio solicitado
         wvResultadosBuscador.loadUrl(lugarIngresado); //Cargando en el navegador el sitio ingresado por el usuario
-        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....",Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
+        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....", Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
     }
+
     public void buscarSugerencia3(View vista) {
-        String lugarIngresado = "https://www.google.com.ec/maps/search/comidas"; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
+        String lugarIngresado = "https://www.google.com.ec/maps/search/comidas+" + this.ciudad; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
         wvResultadosBuscador.setWebViewClient(new WebViewClient()); //Creando un navegador nuevo cada ves que el usuario ingrese un sitio a buscar
         wvResultadosBuscador.getSettings().setJavaScriptEnabled(true); //Habilitando JavaScript dentro de las paginas que se esten buscando
         wvResultadosBuscador.getSettings().setDomStorageEnabled(true); //Habilitando todos los elementos del Doom del sitio solicitado
         wvResultadosBuscador.loadUrl(lugarIngresado); //Cargando en el navegador el sitio ingresado por el usuario
-        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....",Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
+        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....", Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
     }
 
     public void buscarSugerencia4(View vista) {
-        String lugarIngresado = "https://www.google.com.ec/maps/search/gasolineras"; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
+        String lugarIngresado = "https://www.google.com.ec/maps/search/gasolineras+" + this.ciudad; //Capturar el valor ingresado por el usuario en el txtBarraBuscador y almacenamos en la variable ingresada lugarIngresado
         wvResultadosBuscador.setWebViewClient(new WebViewClient()); //Creando un navegador nuevo cada ves que el usuario ingrese un sitio a buscar
         wvResultadosBuscador.getSettings().setJavaScriptEnabled(true); //Habilitando JavaScript dentro de las paginas que se esten buscando
         wvResultadosBuscador.getSettings().setDomStorageEnabled(true); //Habilitando todos los elementos del Doom del sitio solicitado
         wvResultadosBuscador.loadUrl(lugarIngresado); //Cargando en el navegador el sitio ingresado por el usuario
-        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....",Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
+        Toast.makeText(getApplicationContext(), "Cargando el sitio, porfavor espere....", Toast.LENGTH_LONG).show(); //Mostrando en pantalla un mensaje de información
     }
 
     public void cerrarBuscador(View vista) {
--- app/src/main/java/com/utc/appevapar2/MainActivity.java
+++ app/src/main/java/com/utc/appevapar2/MainActivity.java
@@ -2,20 +2,59 @@ package com.utc.appevapar2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText buscadorMain;
+    private CheckBox checkBoxMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buscadorMain = (EditText) findViewById(R.id.txtBuscadorMain);
+        checkBoxMain = (CheckBox) findViewById(R.id.checkBoxMain);
+
+        // Inicializamos SharePreferences
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        buscadorMain.setText(preferences.getString("ciudad", ""));
+
+
+        // Comprobar si tiene datos almacenados para mantener activado el checkbox caso contrario desactivar
+        if (buscadorMain.getText().toString().isEmpty()) {
+            this.checkBoxMain.setChecked(false);
+        } else {
+            this.checkBoxMain.setChecked(true);
+        }
+
     }
+
     // Buscar
-    public void buscar(View vista)
-    {
+    public void buscar(View vista) {
+        //Guardar SharePreferences
+        SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_MULTI_PROCESS);
+        SharedPreferences.Editor editor = preferencias.edit();
+        // Almacenamos si checkbox eactivado caso contrario no
+        if (checkBoxMain.isChecked()) {
+            // Colocar valores
+            editor.putString("ciudad", buscadorMain.getText().toString());
+        } else {
+            // Borrar valores
+            editor.putString("ciudad", "");
+        }
+        // Confirmar cambios del share prerences
+        editor.commit();
+
         //Creando un Intent para invocar a la ventana de Registro
         Intent pantallaBuscar = new Intent(getApplicationContext(), BuscadorActivity.class);
         //Iniciando la pantalla de registro
