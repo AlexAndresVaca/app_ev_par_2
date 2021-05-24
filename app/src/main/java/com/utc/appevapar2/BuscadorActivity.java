4a0547f3acd4e2fa62fcef93c316ca743b7a2970
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2021-05-23 19:57:31 -0500

    Navegacion entre pantallas AVaca

--- app/src/main/java/com/utc/appevapar2/BuscadorActivity.java
+++ app/src/main/java/com/utc/appevapar2/BuscadorActivity.java
@@ -3,6 +3,7 @@ package com.utc.appevapar2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class BuscadorActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class BuscadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscador);
+    }
+
+    public void cerrarBuscador(View vista) {
+        finish(); //Cerrando la pantalla de registro
     }
 }
--- app/src/main/java/com/utc/appevapar2/MainActivity.java
+++ app/src/main/java/com/utc/appevapar2/MainActivity.java
@@ -2,7 +2,9 @@ package com.utc.appevapar2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // Buscar
+    public void buscar(View vista)
+    {
+        //Creando un Intent para invocar a la ventana de Registro
+        Intent pantallaBuscar = new Intent(getApplicationContext(), BuscadorActivity.class);
+        //Iniciando la pantalla de registro
+        startActivity(pantallaBuscar);
     }
 }
